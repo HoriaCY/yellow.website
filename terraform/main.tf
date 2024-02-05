957d2da497fc06ffb5cf9f99eba8f0212eeb91ec
HoriaCY <horia@codeyellow.dev>
2024-02-05 23:09:06 +0200

    remove public bucket policy and update css

--- terraform/main.tf
+++ terraform/main.tf
@@ -62,14 +62,6 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
 
 }
 
-resource "aws_s3_bucket_public_access_block" "s3_bucket_public_access_block" {
-  bucket = aws_s3_bucket.s3_bucket.id
-
-  block_public_acls   = false
-  block_public_policy = false
-}
-
-
 resource "aws_s3_bucket_website_configuration" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
